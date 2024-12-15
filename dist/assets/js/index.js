a63b0999f384fc7a52fb6e4aaf216d534f57fda8
Airi Ichikawa <mmsrm.7911@gmail.com>
2024-12-15 12:43:14 +0900

    fix: js

---
+++ dist/assets/js/index.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("sample1です"),console.log("sample2です")})();
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,5 @@
 import "../sass/style.scss";
+import { sample1 } from "./modules/modules.js"
+sample1();
+import { sample2 } from "./modules/modules2.js"
+sample2();
